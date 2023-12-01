415896fa588bda7ad7de1bb1a40c046f9b89edbb
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-12-01 17:27:30 +0100

    Expand size concepts to extract instance sizes

--- shared/utils/size-concepts.hpp
+++ shared/utils/size-concepts.hpp
@@ -25,6 +25,22 @@ namespace il2cpp_utils {
     template<typename T>
     static constexpr auto il2cpp_size_v = il2cpp_size<T>::value;
 
+    /// @brief extracting the instance size of an il2cpp type wrapper
+    template<typename T>
+    struct il2cpp_instance_size;
+
+    /// @brief ref types should declare a static member __IL2CPP_REFERENCE_TYPE_SIZE that contains their instance size
+    template<::il2cpp_utils::il2cpp_reference_type T>
+    struct il2cpp_instance_size<T> {
+        static constexpr auto value = T::__IL2CPP_REFERENCE_TYPE_SIZE;
+    };
+
+    /// @brief VT wrappers always define their instance size in the lowermost type
+    template<::il2cpp_utils::il2cpp_value_type T>
+    struct il2cpp_instance_size<T> {
+        static constexpr auto value = T::VALUE_TYPE_SIZE;
+    };
+
     /// @brief type trait that checks whether the il2cpp size matches the cpp size
     template<typename T>
     struct is_il2cpp_size_safe {
@@ -34,7 +50,10 @@ namespace il2cpp_utils {
     /// @brief shorthand to get the size check value
     template<typename T>
     static constexpr bool is_il2cpp_size_safe_v = is_il2cpp_size_safe<T>::value;
+
+
 }
 
 #define il2cpp_sizeof(...) (::il2cpp_utils::il2cpp_size<__VA_ARGS__>::value)
 #define il2cpp_safe(...) (::il2cpp_utils::is_il2cpp_size_safe_v<__VA_ARGS__>)
+#define il2cpp_instance_sizeof(...) (::il2cpp_utils::il2cpp_instance_size<__VA_ARGS__>::value)
